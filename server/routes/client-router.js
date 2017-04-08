8dd2c073f04c094cc09052841b31494d5711bf24
HypeUFO <michael.maio22@gmail.com>
2017-04-08 14:54:03 -0700

    create routes and endpoints for users and clients

---
+++ server/routes/client-router.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const morgan = require('morgan');
+
+mongoose.Promise = global.Promise;
+
+
+const { Client } = require('../models/client-model');
+
+// GET CLIENT
+
+router.get('/', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  Client
+    .find({ userId: req.user.id })
+    .exec()
+    .then(clients => {
+      res.json({
+        clients: clients.map(
+          (client) => client.apiRepr())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+router.get('/:id', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  Client
+    .findById(req.params.id)
+    .exec()
+    .then(client =>res.json(client.apiRepr()))
+    .catch(err => {
+      console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+// CREATE CLIENT
+
+router.post('/', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+    Client
+    .create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      company: req.body.company,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+      dateCreated: req.body.dateCreated,
+      dateModified: req.body.dateModified,
+      projects: req.body.projects,
+      userId: req.user.id
+    })
+    .then(
+      client => res.status(201).json(client.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+
+// UPDATE CLIENT
+
+router.put('/:id', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+  const toUpdate = {dateModified: new Date().toISOString()};
+  const updateableFields = ['firstName', 'lastName', 'company', 'email', 'phone', 'address', 'dateModified', 'projects'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Client
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(client => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+// DELETE CLIENT
+
+router.delete('/:id', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  Client
+  .findByIdAndRemove(req.params.id)
+  .exec()
+  .then(client => res.status(204).end())
+  .catch(err => res.status(500).json({message: 'Internal server error'}));
+})
---
+++ server/routes/user-router.js
@@ -0,0 +1,165 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+mongoose.Promise = global.Promise;
+
+
+const { User } = require('../models/user-model');
+
+const localStrategy = new LocalStrategy(function(username, password, callback) {
+  let user;
+  User
+    .findOne({userName: username})
+    .exec()
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return callback(null, false, {message: 'Incorrect username or password'});
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return callback(null, false, {message: 'Incorrect username or password'});
+      }
+      else {
+        return callback(null, user)
+      }
+    });
+});
+
+passport.use(localStrategy);
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// USERS
+
+  // Sign Up/Create User
+
+router.post('/', (req, res) => {
+  if (req.body.password !== req.body.passwordConfirm) {
+    res.status(500).json({
+      message: 'passwords do not match'
+    })
+  }
+  const requiredFields = ['email', 'password'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const encryption = User.hashPassword(req.body.password);
+  User
+    .create({
+      userName: req.body.userName,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+      templates: req.body.templates,
+      dateCreated: req.body.dateCreated,
+      dateModified: req.body.dateModified,
+      hashedPassword: encryption.hash,
+      salt: encryption.salt
+    })
+    .then(user => res.status(200).json(user.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        message: 'something went wrong',
+        error: err
+      });
+    });
+});
+
+  // User Login
+
+router.get('/dashboard', (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.redirect('/login');
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  res.status(200).redirect('/dashboard', {
+    user: req.user.apiRepr()
+  });
+})
+
+router.post('/login',
+  passport.authenticate('local', {session: true, successRedirect: '/dashboard', failureRedirect: '/login', failureFlash: 'Incorrect username or password'}),
+  (req, res) => {
+    // if (req.body.userName === 'demo' && req.isAuthenticated()) {
+  //   //seedDemoInfo();
+  // }
+  });
+
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+// UPDATE USER
+
+router.put('/:id', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+  const toUpdate = {dateModified: new Date().toISOString()};
+  const updateableFields = ['firstName', 'lastName', 'userName', 'email', 'phone', 'address', 'dateModified', 'templates'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  User
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(user => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+// DELETE USER
+
+router.delete('/:id', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  User
+  .findByIdAndRemove(req.params.id)
+  .exec()
+  .then(user => res.status(204).end())
+  .catch(err => res.status(500).json({message: 'Internal server error'}));
+})
