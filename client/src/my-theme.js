c0da695e6c0231a14ebde48bda0815b7c1755327
HypeUFO <michael.maio22@gmail.com>
2017-04-08 14:56:10 -0700

    remove unused module imports

--- client/src/my-theme.js
+++ client/src/my-theme.js
@@ -1,6 +1,3 @@
-import React from 'react';
-// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 
