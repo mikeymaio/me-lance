5d32482a7fd7755602e50f158b13f70e08b9e102
HypeUFO <michael.maio22@gmail.com>
2017-04-08 12:29:09 -0700

    remove duplicate client folder

--- freelancer/src/menu/left-menu.actions.js
+++
@@ -1,7 +0,0 @@
-export function handleMenuItemSelect(menuItem) {
-    const UPDATE_SELECTION = 'MENU_ITEM_SELECTED';
-    return {
-        type: UPDATE_SELECTION,
-        payload: menuItem
-    }
-}
