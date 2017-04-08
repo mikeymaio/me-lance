499b8091217aa9d1d8e98dc28b3739a92e27ba9c
HypeUFO <michael.maio22@gmail.com>
2017-04-08 14:59:10 -0700

    remove unused module imports

--- client/src/landing/landing.component.js
+++ client/src/landing/landing.component.js
@@ -1,17 +1,8 @@
 import React, {Component} from 'react';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-// import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
 // import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-import DrawerMenu from '../drawer/drawer.component';
-
 import LoginModal from '../login/login-modal.component';
-import TimeTracker from '../time-tracker/time-tracker.component';
 
 import * as actions from '../header/header.actions';
 import { connect } from 'react-redux';
