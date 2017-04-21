7cf4bedf748af7ecbe7995944f3ba21b9281cd60
HypeUFO <michael.maio22@gmail.com>
2017-04-21 15:06:07 -0700

    move database url

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
+exports.DATABASE_URL = 'mongodb://mmaio:asdyoug7@ds055575.mlab.com:55575/freelancer-db' ||
+                        process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://mmaio:asdyoug7@ds055575.mlab.com:55575/freelancer-db';
+                      'mongodb://localhost/freelancer-db';
 
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
