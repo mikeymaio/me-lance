8fa258a5fa2a89f7e135385f36ac103c216896b4
HypeUFO <michael.maio22@gmail.com>
2017-05-02 10:16:37 -0700

    center loader

--- client/src/loader/loader.component.js
+++ client/src/loader/loader.component.js
@@ -6,7 +6,7 @@ const Loader = () => (
   <div>
     {/*<CircularProgress />*/}
     {/*<CircularProgress size={60} thickness={7} />*/}
-    <Paper style={{height: 200, width: 200, backgroundColor: "#eee", position: "absolute", left: "42%", top: "40%", zIndex: 5000}}
+    <Paper style={{height: 200, width: 200, backgroundColor: "#eee", position: "absolute", left: "42%", top: "35%", zIndex: 5000}}
     children={
     <CircularProgress size={100} thickness={5} style={{top: "25%", left:"25%"}} />
     } />
