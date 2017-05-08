c77de9b1f44326e5db1c3540de029bcbc560faa7
HypeUFO <michael.maio22@gmail.com>
2017-05-08 12:29:40 -0700

    add store to app.test

--- client/src/app/app.test.js
+++ client/src/app/app.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './app';
+import store from '../store';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import myTheme from '../my-theme';
+
+import '../index.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+  <MuiThemeProvider muiTheme={myTheme} >
+    <App />
+  </MuiThemeProvider>
+  </Provider>,
+  div
+  )
 });
